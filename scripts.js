e4f0ee2fa089184fbd31fd5e93830868d08d64b8
unknown <rosiek.kuba@gmail.com>
2017-07-31 16:19:26 +0100

    do poprawy

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+var list = document.getElementById('list'); //zapamiętuje w zmiennej list odniesienie do id list
+var add = document.getElementById('addElem'); //nadaje listener na przycisk
+	add.addEventListener('click', function() { //funkcja reaguje na klik 
+		var element = document.createElement('li'); // tworzy kolejne pozycje menu
+		element.innerHTML = 'item'; // przypisujemy tekst item do nowej liisty
+		document.getElementsByTagName.length('element'); // pokazuje aktualna ilosc listy
+		list.appendChild(element); // dodaje numer do textu 'li'
+		console.log(list.appendChild);
+	}; 

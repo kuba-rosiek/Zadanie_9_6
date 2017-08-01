8f167124ffd0e55364120bbe6ae19ac229358b56
kuba-rosiek <rosiek.kuba@gmail.com>
2017-08-01 10:36:38 +0100

    Update scripts.js
    Poprawione, dziala

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
 var list = document.getElementById('list'); //zapamiętuje w zmiennej list odniesienie do id list
 var add = document.getElementById('addElem'); //nadaje listener na przycisk
-	add.addEventListener('click', function() { //funkcja reaguje na klik 
+	add.addEventListener('click', function() 
+	{ //funkcja reaguje na klik 
 		var element = document.createElement('li'); // tworzy kolejne pozycje menu
-		element.innerHTML = 'item'; // przypisujemy tekst item do nowej liisty
-		document.getElementsByTagName.length('element'); // pokazuje aktualna ilosc listy
-		list.appendChild(element); // dodaje numer do textu 'li'
-		console.log(list.appendChild);
-	}); 
+			element.innerHTML = 'item'+ document.getElementsByTagName('li').length;; // przypisujemy tekst z numerem 
+			list.appendChild(element); // dodaje item z numerem
+			
+	});
